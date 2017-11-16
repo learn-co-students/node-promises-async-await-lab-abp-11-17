2ff0719ab4a54a65eb38ab8d00571329958b4e28
jencekade <gaephuoc@gmail.com>
2017-11-16 03:14:43 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -1,14 +1,14 @@
 'use strict';
 
 class Question {
-  constructor(content){
+  constructor(content) {
     this.content = content;
   }
 
-  static All(){
+  static All() {
     console.log("Waiting for Questions to load...")
-    return new Promise(function(resolve){
-      setTimeout(function(){
+    return new Promise(function(resolve) {
+      setTimeout(function() {
         const questions = Question._All
         console.log("...Questions Loaded")
 
@@ -17,31 +17,31 @@ class Question {
     })
   }
 
-  static async PrintAll(){
+  static async PrintAll() {
     const allQuestions = await Question.All()
     console.log("\n")
-    allQuestions.forEach(function(question){
+    allQuestions.forEach(function(question) {
       console.log(question.content)
     })
     console.log("\n")
   }
 
   // FIXME: Implement Promise
-  static Find(id){
+  static Find(id) {
     console.log(`Waiting for Question ${id} to load...`)
-
-      setTimeout(function(){
-        const question = Question._All[id-1] // Do Not Edit
+    return new Promise(function(resolve) {
+      setTimeout(function() {
+        const question = Question._All[id - 1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
 
-        return question
+        resolve (question)
       }, 1500)
-
+    })
   }
 
   // FIXME: Implement async / await
-  static Print(id){
-    const question = Question.Find(id)
+  static async Print(id) {
+    const question = await Question.Find(id);
 
     console.log("\n")
     console.log(question.content)
