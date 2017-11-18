c0837271c528a7fa0b429225e2f904435a9e657d
Tim Shipman <bricklayertim@yahoo.com>
2017-11-18 00:22:51 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -29,19 +29,19 @@ class Question {
   // FIXME: Implement Promise
   static Find(id){
     console.log(`Waiting for Question ${id} to load...`)
-
+    return new Promise(function(resolve){
       setTimeout(function(){
         const question = Question._All[id-1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
 
-        return question
+        resolve (question)
       }, 1500)
-
+    })
   }
 
   // FIXME: Implement async / await
-  static Print(id){
-    const question = Question.Find(id)
+  static async Print(id){
+    const question = await Question.Find(id)
 
     console.log("\n")
     console.log(question.content)
@@ -49,10 +49,14 @@ class Question {
   }
 }
 
+
+
 Question._All = [
   new Question("Where in the world is Carmen Sandiego?"),
   new Question("What's your favorite TV Show?"),
   new Question("What superpower would you want?")
 ]
+
+
 
 module.exports = Question
